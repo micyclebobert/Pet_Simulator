16a9e8cfdd88322cfe7b4b7f5f51d76b8e111797
micyclebobert <atulpurna@gmail.com>
2024-10-31 19:50:59 +0600

    tst 2

--- src/main/java/com/cse215/g2/pet_simulator/CustomMethods.java
+++ src/main/java/com/cse215/g2/pet_simulator/CustomMethods.java
@@ -1,21 +1,29 @@
 package com.cse215.g2.pet_simulator;
 
 import java.awt.Image;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
 
 public class CustomMethods {
-    public static URL getFullPathURL(String localPath) {
-        return Pet_Simulator.class.getResource(localPath);
+    public static String getFullPathString(String localPath) {
+        return Pet_Simulator.class.getResource(localPath).toString();
     }
 
     public static ImageIcon getImageIconFromLocalPath(String localPath) {
-        return new ImageIcon(getFullPathURL(localPath));
+        return new ImageIcon(getFullPathString(localPath));
     }
 
     public static Image getImageFromLocalPath(String localPath) {
-        return new ImageIcon(getFullPathURL(localPath)).getImage();
+        return new ImageIcon(getFullPathString(localPath)).getImage();
     }
 
     public static Image getScaledImage(Image image, int width, int height, int hint) {
@@ -37,4 +45,43 @@ public class CustomMethods {
     public static ImageIcon getScaledImageIcon(ImageIcon imageIcon, int width, int height, int hint) {
         return new ImageIcon(getScaledImage(imageIcon, width, height, hint));
     }
+
+
+    public static void saveObjectToFile(String localPath, Object data) throws Exception{
+        ObjectOutputStream objectOutputStream=new ObjectOutputStream(new FileOutputStream(getFullPathString(localPath)));
+        objectOutputStream.writeObject(data);
+        objectOutputStream.close();
+        /*
+        try (ObjectOutputStream write = new ObjectOutputStream(new FileOutputStream(path))) {
+            write.writeObject(data);
+        } catch (NotSerializableException nse) {
+            // do something
+        } catch (IOException eio) {
+            // Failed to read data
+        }*/
+    }
+
+    public static void loadSettingsValue() {
+        //Settings settings=null;
+        new File(getFullPathString("SettingsValue"));
+        //Settings s=new Settings();
+        try{
+Settings s=(Settings)loadObjectFromFile("SettingsValue");
+        } catch(Exception e){
+            e.printStackTrace();
+            
+        }
+    }
+    public static Object loadObjectFromFile(String localPath) throws Exception {
+        ObjectInputStream objectInputStream=new ObjectInputStream(new FileInputStream(getFullPathString(localPath)));
+        Object object=objectInputStream.readObject();
+        objectInputStream.close();
+        return object;
+        /*try (ObjectInputStream inFile = new ObjectInputStream(new FileInputStream(path))) {
+            return inFile.readObject();
+        } catch (Exception e) {
+            return null;
+        }*/
+    }
+
 }
--- src/main/java/com/cse215/g2/pet_simulator/Pet_Simulator.java
+++ src/main/java/com/cse215/g2/pet_simulator/Pet_Simulator.java
@@ -1,20 +1,19 @@
 package com.cse215.g2.pet_simulator;
 
-
 public class Pet_Simulator {
-//Background, Toys, Pet, Action, UI
-//If action is going on and new action
-// then cancel the current and start new
-//Actions update variables after it is over
+    // Background, Toys, Pet, Action, UI
+    // If action is going on and new action
+    // then cancel the current and start new
+    // Actions update variables after it is over
     public static void main(String[] args) {
         Validate();
         GUI.start();
-        Animal a=new Animal();
-        System.out.println("Hello World!"+a.age);
+        Animal a = new Animal();
+        System.out.println("Hello World!" + a.age);
     }
-    public static void Validate()
-    {
+
+    public static void Validate() {
         //
-        
+
     }
 }
