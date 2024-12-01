c630558050367bdfdf1c2a724470e72a79b7a031
micyclebobert <atulpurna@gmail.com>
2024-12-01 18:26:12 +0600

    small change

--- src/main/java/com/cse215/g2/pet_simulator/GUIManager.java
+++ src/main/java/com/cse215/g2/pet_simulator/GUIManager.java
@@ -5,7 +5,7 @@ public class GUIManager implements Runnable {
     @Override
     public void run() {
         new MainMenu();
-        new GameGUI(new Animal("A"));
+        new GameGUI(new Animal("Ani"));
     }
     
 }
